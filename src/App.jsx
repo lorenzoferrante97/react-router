1362a8881e7cb8eb0de0b808408e1b34c5552953
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-28 11:58:48 +0100

    add: pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@
 // import './App.css'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import DefaultLayout from './layouts/DefaultLayout'
+import DefaultLayout from './layouts/DefaultLayout';
+import Homepage from './pages/Homepage';
+import AboutUs from './pages/AboutUs';
+import Posts from './pages/Posts';
 
 function App() {
 
@@ -14,9 +17,12 @@ function App() {
         {/* layout */}
         <Route Component={ DefaultLayout }>
 
+          <Route to="/" Component={ Homepage }></Route>
+          <Route to="/about-us" Component={ AboutUs }></Route>
+          <Route to="/posts" Component={ Posts }></Route>
 
         </Route>
-        
+
       </Routes>
     </BrowserRouter>
 
