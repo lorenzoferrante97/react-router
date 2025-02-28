062a8d0c8e244980ad8c60dc4375d05a59c44911
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-28 11:54:12 +0100

    add: router structure + layout comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 // import { useState } from 'react'
 // import './App.css'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DefaultLayout from './layouts/DefaultLayout'
+
 function App() {
 
   return (
 
-    <>
-    
-    </>
+    <BrowserRouter>
+      <Routes>
+
+        {/* layout */}
+        <Route Component={ DefaultLayout }>
+
+
+        </Route>
+        
+      </Routes>
+    </BrowserRouter>
 
   )
 }
