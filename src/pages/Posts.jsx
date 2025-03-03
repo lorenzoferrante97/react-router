672b1a545a279c0454fb61c4834ecad7f8f9e21c
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-03 12:02:42 +0100

    add: usestate + useffect

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,18 +1,21 @@
+import { useEffect, useState } from 'react';
 
+const { postsList, setPostsList } = useState({ id: 0, title: '', content: '', image: '', tags: [] });
+
+useEffect(() => {}, []);
 
 export default function Posts() {
+  return (
+    <main className="container-fluid bg-smoke-50 mt-10u rounded-xl">
+      <section className="row-grid">
+        {/* ciclare cols con dentro posts */}
 
-    return (
-
-        <main className="container-fluid bg-smoke-50 mt-10u rounded-xl">
-            <section className="row-grid">
-                <div className="col-span-full flex perfect-center min-h-[60vh]">
-                    <h1 className="font-h1">Titolo Posts</h1>
-                </div>
-            </section>
-        </main>
-
-    );
-};
+        <div className="col-span-full flex perfect-center min-h-[60vh]">
+          <h1 className="font-h1">Titolo Posts</h1>
+        </div>
+      </section>
+    </main>
+  );
+}
 
 // end code
