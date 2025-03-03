4fc707d7d730156fc2bf95b73c79aa18648c746e
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-03 13:58:18 +0100

    add: fetch api for single post

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 export default function Post() {
+  const { post, setPost } = useState({ id: 0, title: '', content: '', image: '', tags: [] });
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/posts/${id}`)
+      .then((response) => response.json())
+      .then((data) => {})
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+
   return (
     <main className="container-fluid bg-smoke-50 mt-10u rounded-xl">
-      <section className="row-grid">
-        <div className="col-span-full flex perfect-center min-h-[60vh]">
-          <h1 className="font-h1">Titolo Post singolo</h1>
-        </div>
-      </section>
+      <section className="row-flex"></section>
     </main>
   );
 }
