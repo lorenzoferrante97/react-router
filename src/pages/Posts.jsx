fda15c7e5ecb9abca0a115f9a99b4714dd1da9ab
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-03 12:37:45 +0100

    add: post info on page

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,27 +1,52 @@
 import { useEffect, useState } from 'react';
 
 export default function Posts() {
-  const { postsList, setPostsList } = useState({ id: 0, title: '', content: '', image: '', tags: [] });
+  const [postsList, setPostsList] = useState([]);
 
   useEffect(() => {
-    fetch('url API')
+    fetch('http://localhost:3000/posts')
       .then((response) => response.json())
-      .then((data) => {
-        setPostsList(data);
-      })
+      .then((data) => setPostsList(data))
       .catch((error) => {
         console.error(error);
       });
-  }, []);
+  });
 
   return (
     <main className="container-fluid bg-smoke-50 mt-10u rounded-xl">
       <section className="row-grid">
         {/* ciclare cols con dentro posts */}
 
-        <div className="col-span-full flex perfect-center min-h-[60vh]">
-          <h1 className="font-h1">Titolo Posts</h1>
-        </div>
+        {postsList.map((singlePost) => {
+          const { id, title, content, image, tags } = singlePost;
+
+          return (
+            <>
+              <div
+                className="col-span-full flex flex-col perfect-center min-h-[60vh] gap-7u"
+                key={id}
+              >
+                <span className="w-full h-28u flex perfect-center bg-smoke-100">{image}</span>
+                <h1 className="font-h1">{title}</h1>
+                <div className="flex gap-2u">
+                  {tags.map((singleTag, i) => {
+                    return (
+                      <>
+                        <span
+                          className="px-3u py-2u font-body-s-regular border border-black rounded-full"
+                          key={i}
+                        >
+                          {singleTag}
+                        </span>
+                      </>
+                    );
+                  })}
+                </div>
+                <p className="font-body-base-light">{content}</p>
+              </div>
+            </>
+          );
+        })}
       </section>
     </main>
   );
