d56447b0535551811e2b06a87a34c85f31549363
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-03 11:54:53 +0100

    add: single post page

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,30 @@ import DefaultLayout from './layouts/DefaultLayout';
 import Homepage from './pages/Homepage';
 import AboutUs from './pages/AboutUs';
 import Posts from './pages/Posts';
+import Post from './pages/Post';
 
 function App() {
-
   return (
-
     <BrowserRouter>
       <Routes>
-
         {/* layout */}
-        <Route Component={ DefaultLayout }>
-
-          <Route path="/" Component={ Homepage }></Route>
-          <Route path="/about-us" Component={ AboutUs }></Route>
-          <Route path="/posts" Component={ Posts }></Route>
-
+        <Route Component={DefaultLayout}>
+          <Route
+            path="/"
+            Component={Homepage}></Route>
+          <Route
+            path="/about-us"
+            Component={AboutUs}></Route>
+          <Route
+            path="/posts"
+            Component={Posts}></Route>
+          <Route
+            path="/posts/:id"
+            Component={Post}></Route>
         </Route>
-
       </Routes>
     </BrowserRouter>
-
-  )
+  );
 }
 
 export default App;
---
+++ src/pages/Post.jsx
@@ -0,0 +1,11 @@
+export default function Post() {
+  return (
+    <main className="container-fluid bg-smoke-50 mt-10u rounded-xl">
+      <section className="row-grid">
+        <div className="col-span-full flex perfect-center min-h-[60vh]">
+          <h1 className="font-h1">Titolo Post singolo</h1>
+        </div>
+      </section>
+    </main>
+  );
+}
